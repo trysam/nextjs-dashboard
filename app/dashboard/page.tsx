3e0f2a259d2632aa1b6f2729d1870a1239452b5f
Samson Awokunle <trysam2003@gmail.com>
2026-06-11 09:41:07 +0100

    Dashboard data added

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,16 +1,37 @@
-import { lusitana } from "../ui/fonts";
+import { Card } from "@/app/ui/dashboard/cards";
+import RevenueChart from "@/app/ui/dashboard/revenue-chart";
+import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
+import { lusitana } from "@/app/ui/fonts";
+import { fetchRevenue, fetchLatestInvoices, fetchCardData } from "../lib/data";
 
-export default function Page() {
+export default async function Page() {
+  const revenue = await fetchRevenue();
+  const latestInvoices = await fetchLatestInvoices();
+  const {
+    totalPaidInvoices,
+    totalPendingInvoices,
+    numberOfInvoices,
+    numberOfCustomers,
+  } = await fetchCardData();
+
   return (
-    <main className="flex min-h-screen flex-col">
-      <div className="flex flex-col h-38 md:h-52 gap-4 shrink-0 rounded-lg bg-blue-500 p-4">
-        <p className={`${lusitana.className} text-white text-3xl font-bold`}>
-          Dashboard Page
-        </p>
-        <p className="text-slate-100 text-sm md:text-base">
-          Here is the dashboard page you have been looking for all this while.
-          Make sure you make a good use of it
-        </p>
+    <main>
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+        Dashboard
+      </h1>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        <Card title="Collected" value={totalPaidInvoices} type="collected" />
+        <Card title="Pending" value={totalPendingInvoices} type="pending" />
+        <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
+        <Card
+          title="Total Customers"
+          value={numberOfCustomers}
+          type="customers"
+        />
+      </div>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        <RevenueChart revenue={revenue} />
+        <LatestInvoices latestInvoices={latestInvoices} />
       </div>
     </main>
   );
